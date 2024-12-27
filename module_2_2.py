b40a44d804c91216a3b674efa32cdb55a4055238
DelaaRich <eugenes2016.ru@gmail.com>
2024-12-27 18:23:53 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+(first, second, third) = input("Введите число: ")
+if first == second and first == third:
+    print(3)
+elif first == second or first == third:
+    print(2)
+else:
+    print(0)
+
+
